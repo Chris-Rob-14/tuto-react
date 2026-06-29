bd1c4003b42f4f8b3a0bcf10ad74b1616960ef80
Chris <christopher.robine@ca-cheques.credit-agricole.fr>
2026-06-29 12:26:38 +0200

    ✨ Add button

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+/*
+function App() {
+  return <p>DEMAT BREIZH</p>
+}
+*/
+
+import Button from "../uis/Button/Button";
+
+const App=()=>{return <>
+  <p>DEMAT BREIZH</p> 
+  <Button />
+  </>
+};
+
+export default App;
+
